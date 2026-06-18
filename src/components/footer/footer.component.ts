5ba39fc1888fe85a30128f09b182beccc8388294
dianaabeja <dianabeja00@gmail.com>
2026-06-18 11:35:20 -0600

    cambios

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -16,9 +16,9 @@ export class FooterComponent {
   email = 'bagdiana03@gmail.com';
 
   socials: SocialLink[] = [
-    { label: 'GitHub',    icon: 'pi pi-github',   url: 'https://github.com' },
-    { label: 'LinkedIn',  icon: 'pi pi-linkedin',  url: 'https://linkedin.com' },
-    { label: 'Instagram', icon: 'pi pi-instagram', url: 'https://instagram.com' },
+    { label: 'GitHub',    icon: 'pi pi-github',   url: 'https://github.com/dianaabeja' },
+    { label: 'LinkedIn',  icon: 'pi pi-linkedin',  url: 'https://www.linkedin.com/in/dianalaurabautistagarcia/' },
+    { label: 'Instagram', icon: 'pi pi-instagram', url: 'https://www.instagram.com/diana_abeja/' },
   ];
 
   currentYear = new Date().getFullYear();
